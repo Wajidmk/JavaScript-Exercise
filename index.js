2572fbc80cd504bca7876d1aa93227e88d23a7bf
Abdul Waajid Ahmed <106549884+Wajidmk@users.noreply.github.com>
2024-07-04 21:23:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+let firstName = " Abdul "
+let lastName = " Wajid"
+
+let fullName =  firstName + lastName
+
+
+console.log(fullName)
+
+
+let name = " Linda"
+let greeting = "Hi there"
+
+
+
+function greetingLinda(){
+    console.log(greeting + " ,"+ name)
+}
+
+greetingLinda()
