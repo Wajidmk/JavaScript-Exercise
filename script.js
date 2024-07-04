4704048c6263c248ba663a726b34070a196f027a
Abdul Waajid Ahmed <106549884+Wajidmk@users.noreply.github.com>
2024-07-04 21:21:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
+let myPoints = 3
 
-let firstName = " Abdul "
-let lastName = " Wajid"
+// Create two functions, add3Points() and remove1Point(), and have them
+// add/remove points to/from the myPoints variable
 
-let fullName =  firstName + lastName
-
-
-console.log(fullName)
-
-
-// Exercise 2 
-
-let name = " Linda"
-let greeting = "Hi there"
-
-
-
-function greetingLinda(){
-    console.log(greeting + " ,"+ name)
+function add3Points() {
+    myPoints += 3
 }
 
-greetingLinda()
+function remove1Points() {
+    myPoints -= 1
+}
 
+add3Points()
+add3Points()
+add3Points()
+remove1Points()
+remove1Points()
 
- 
+// Call the functions to that the line below logs out 10
+console.log(myPoints)
 
-
-
-
-
+// Try to predict what each of the lines will log out
+console.log("2" + 2) //
+console.log(11 + 7) //
+console.log(6 + "5") //
+console.log("My points: " + 5 + 9) //
+console.log(2 + 2) // 
+console.log("11" + "14") // 
